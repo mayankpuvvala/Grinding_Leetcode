5b1ac3959549128b156d1fa18133d1e063493c93
mayank puvvala <puvvalamayank@gmail.com>
2024-08-21 00:18:51 +0530

    Time: 3 ms (97.24%) | Memory: 10 MB (11.83%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,15 +3,15 @@ public:
     bool isAnagram(string s, string t) {
         unordered_map<int,int> mp;
 
-        for(auto it: s){
+        for(auto &it: s){
             mp[it]++;
         }
 
-        for(auto i: t){
+        for(auto &i: t){
             mp[i]--;
         }
 
-        for(auto i: mp){
+        for(auto &i: mp){
             if(i.second!=0){
                 return false;
             }
