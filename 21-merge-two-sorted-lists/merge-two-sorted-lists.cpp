488493b3d87aad0557fa20523881f3152c43ed84
mayank puvvala <puvvalamayank@gmail.com>
2024-08-22 08:47:37 +0530

    Time: 5 ms (50.19%) | Memory: 19.7 MB (40.45%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,45 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-       
-        if(list1 == NULL)
+        if(list1==NULL){
             return list2;
-		
-        if(list2 == NULL)
+        }
+        if(list2==NULL){
             return list1;
-        
-        ListNode * ptr = list1;
-        if(list1 -> val > list2 -> val)
-        {
-            ptr = list2;
-            list2 = list2 -> next;
         }
-        else
-        {
-            list1 = list1 -> next;
+        ListNode *ptr= list1;
+        if(list1->val > list2->val){
+            ptr= list2;
+            list2=list2->next;
         }
-        ListNode *curr = ptr;
+        else{
+            list1=list1->next;
+        }
         
-        while(list1 &&  list2)
-        {
-            if(list1 -> val < list2 -> val){
-                curr->next = list1;
-                list1 = list1 -> next;
+        ListNode* curr= ptr;
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val > list2->val){
+                curr->next=list2;
+                list2= list2->next;
             }
             else{
-                curr->next = list2;
-                list2 = list2 -> next;
+                curr->next= list1;
+                list1= list1->next;
             }
-            curr = curr -> next;
-                
+            curr= curr->next;
         }
-        
-        if(!list1)
-            curr -> next = list2;
-        else
-            curr -> next = list1;
-            
+        if(!list1){
+            curr->next= list2;
+        }
+        if(!list2){
+            curr->next=list1;
+        }
         return ptr;
-       
+
+        
     }
 };
