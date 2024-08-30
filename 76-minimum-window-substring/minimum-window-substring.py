d908a7a0bbc6d81e8f3112a2069db783a8f19368
mayank puvvala <puvvalamayank@gmail.com>
2024-08-30 12:33:55 +0530

    Time: 173 ms (34.51%) | Memory: 17.3 MB (22.76%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,14 +1,43 @@
 class Solution:
-    def minWindow(self, s, t):
-        need, missing = collections.Counter(t), len(t)
-        i = I = J = 0
-        for j, c in enumerate(s, 1):
-            missing -= need[c] > 0
-            need[c] -= 1
-            if not missing:
-                while i < j and need[s[i]] < 0:
-                    need[s[i]] += 1
-                    i += 1
-                if not J or j - i <= J - I:
-                    I, J = i, j
-        return s[I:J]
+    def minWindow(self, s: str, t: str) -> str:
+
+        if not s or not t:
+            return ''
+        t_count= Counter(t)
+
+        required= len(t_count)
+
+        min_len= float('inf')
+        min_window=''
+
+        window_counts={}
+
+        formed= 0
+
+        left=right=0
+
+        while  right<len(s):
+            char = s[right]
+
+            window_counts[char]=window_counts.get(char, 0)+1
+
+            if char in t_count and t_count[char]==window_counts[char] :
+                formed+=1
+            
+            while left<=right and formed == required:
+
+                char = s[left]
+
+                if right-left+1 < min_len:
+                    min_len = right-left +1
+                    min_window= s[left:right +1]
+                
+                window_counts[char]-=1
+                if char in t_count and window_counts[char] < t_count[char]:
+                    formed-=1
+                    
+                left+=1
+            right+=1
+        return min_window
+
+        
