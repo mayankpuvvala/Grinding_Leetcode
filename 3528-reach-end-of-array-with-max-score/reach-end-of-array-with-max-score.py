c3bd3ae89383cbde6e3f0e72ce0852b7472058a8
mayank puvvala <puvvalamayank@gmail.com>
2024-09-08 23:07:46 +0530

    Time: 1181 ms (100.00%) | Memory: 31.6 MB (100.00%) - LeetSync

--- 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.py
+++ 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.py
@@ -3,15 +3,13 @@ class Solution:
         score= 0
         i,j=0,nums[0]
         n= len(nums)
-        prev= nums[0]
         start= 0
 
         for i in range(len(nums)):
             
-            if nums[i]>prev:
+            if nums[i]>nums[start]:
                 score+=(i-start)*nums[start]
                 start = i
-                prev=nums[i]
         score+=((n-1)-start)*nums[start]
 
         return score
