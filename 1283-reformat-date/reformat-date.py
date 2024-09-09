8e3af70cb7906b1ffb83b2e15caeff56941baadd
mayank puvvala <puvvalamayank@gmail.com>
2024-09-09 05:53:52 +0530

    Time: 26 ms (94.52%) | Memory: 16.4 MB (70.16%) - LeetSync

---
+++ 1283-reformat-date/reformat-date.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        s= date.split()
+
+        month_dict= {
+            'Jan':'01','Feb':'02',
+            'Mar':'03','Apr':'04',
+            'May':'05','Jun':'06',
+            'Jul':'07','Aug':'08',
+            'Sep':'09','Oct':'10',
+            'Nov':'11','Dec':'12'
+        }
+
+        day= s[0][:-2]
+        month= s[1]
+        year=s[2]
+
+        if int(day)<10:
+            day= '0'+day
+
+        return ''.join(f'{year}-{month_dict[month]}-{day}')
