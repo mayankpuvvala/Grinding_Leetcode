446b61b6e6752fa426c6654035ec2bdd506c4d6c
mayank puvvala <puvvalamayank@gmail.com>
2026-05-22 22:24:08 +0530

    Time: 337 ms (26.07%) | Memory: 138.2 MB (13.23%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,12 +1,11 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        total = sum(piles)
-        from functools import lru_cache
-        @lru_cache(None)
+        @cache
         def traverse(i,j):
             if i>j:
                 return 0
             return max(
-            piles[i]- traverse(i+1, j) ,
-            piles[j]- traverse(i, j-1))
-        return traverse(0,len(piles)-1)>0
+                    piles[i]- traverse(i+1, j),
+                    piles[j]- traverse(i,j-1)) #idea is that don't take the curr element again in the scope, hence, shifting the pointers
+        return traverse(0,len(piles)-1) > 0
+        #or just return True
