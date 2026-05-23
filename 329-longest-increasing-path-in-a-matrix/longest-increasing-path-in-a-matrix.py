0b52f89ca51f873b3c0f561c7550c86840e56e25
mayank puvvala <puvvalamayank@gmail.com>
2026-05-23 09:43:43 +0530

    Time: 311 ms (5.01%) | Memory: 111.1 MB (5.16%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -1,20 +1,18 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         rows, cols = len(matrix), len(matrix[0])
-        dp = {}
-        def dfs(i, j, val):
-            if i<0 or j<0 or i==rows or j==cols:
+        @cache
+        def dfs(i,j, prev):
+            if i<0 or j<0 or i>=rows or j>=cols or  matrix[i][j]<=prev: 
                 return 0
-            if val>=matrix[i][j]:
-                return 0
-            if (i,j) in dp:
-                return dp[(i,j)]
-            res= 1+max(
-                dfs(i+1, j, matrix[i][j]),  
-                dfs(i, j+1, matrix[i][j]),  
-                dfs(i-1, j, matrix[i][j]), 
-                dfs(i, j-1, matrix[i][j]) 
-            )
-            dp[(i,j)] = res
-            return res
-        return max(dfs(i,j, float('-inf')) for i in range(rows) for j in range(cols))
+            prev= matrix[i][j]
+            return 1+ max(
+            dfs(i+1, j, prev),
+            dfs(i-1, j, prev),
+            dfs(i, j-1, prev),
+            dfs(i, j+1, prev) )
+        ans= 0
+        for i in range(rows):
+            for j in range(cols):
+                ans= max(ans, dfs(i,j, float("-inf")))
+        return ans
