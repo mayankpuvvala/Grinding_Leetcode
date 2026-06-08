918d071ad4a8f4ae6fb72112b03a79e5ed3ffd82
mayank puvvala <puvvalamayank@gmail.com>
2026-06-08 23:07:44 +0530

    Time: 28 ms (79.03%) | Memory: 22.8 MB (9.41%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,24 +1,22 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        adj = [[] for _ in range(numCourses)]
-        indegree = [0] * numCourses
-        for p in prerequisites:
-            adj[p[0]].append(p[1])
-            indegree[p[1]] += 1
-        
-        q = deque()
+        directions = defaultdict(list)
+        for j,i in prerequisites:
+            directions[i].append(j)
+        preMap = {}
+        def dfs(i):
+            if i not in preMap:
+                preMap[i] = set()
+                for j in directions[i]:
+                    preMap[i] |= dfs(j)
+                preMap[i].add(i)
+            return preMap[i]
+
         for i in range(numCourses):
-            if indegree[i] == 0:
-                q.append(i)
+            dfs(i)
         
-        mp = defaultdict(set)
-        while q:
-            curr = q.popleft()
-            for next_course in adj[curr]:
-                mp[next_course].add(curr)
-                mp[next_course].update(mp[curr])
-                indegree[next_course] -= 1
-                if indegree[next_course] == 0:
-                    q.append(next_course)
+        res = []
+        for u,v in queries:
+            res.append(u in preMap[v])
+        return res
         
-        return [q[0] in mp[q[1]] for q in queries]
