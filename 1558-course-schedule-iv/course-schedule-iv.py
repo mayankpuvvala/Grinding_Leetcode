5774d6d142ef03056d9b9a7f3bbd8d94460bd439
mayank puvvala <puvvalamayank@gmail.com>
2026-06-13 22:26:58 +0530

    Time: 22 ms (92.95%) | Memory: 22.7 MB (14.22%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,22 +1,24 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        directions = defaultdict(list)
-        for j,i in prerequisites:
-            directions[i].append(j)
-        preMap = {}
+        adj= defaultdict(list)
+        for i,j in prerequisites:
+            adj[i].append(j)
+        # 0:[], 1: [0,2], 2: [0]
+        # if 1, we'll check all prereq and their prereq if any, and if they have the value 1, then we return False
+        visited= set()
+        carries = defaultdict(set)
         def dfs(i):
-            if i not in preMap:
-                preMap[i] = set()
-                for j in directions[i]:
-                    preMap[i] |= dfs(j)
-                preMap[i].add(i)
-            return preMap[i]
+            if i in visited:
+                return
+            visited.add(i)
+            for nei in adj[i]:
+                dfs(nei)
+                carries[i].add(nei)
+                carries[i].update(carries[nei]) #bubble backward, by adding the nei connections to i
 
         for i in range(numCourses):
             dfs(i)
-        
-        res = []
-        for u,v in queries:
-            res.append(u in preMap[v])
+        res= []
+        for i,j in queries:
+            res.append(j in carries[i])
         return res
-        
