f200fe14546a59bfd5a26461754344471c4c3b23
mayank puvvala <puvvalamayank@gmail.com>
2026-06-23 22:09:21 +0530

    Time: 49 ms (16.31%) | Memory: 20.3 MB (43.37%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res= []
+        for i in range(n+1):
+            count= 0
+            while i:
+                count+= i%2
+                i//=2
+            res.append(count)
+        return res
